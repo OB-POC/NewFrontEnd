8ff5920fbe5d0027615d3054ba690e8cecfd871e
Shaik Aqib (Digital) <SH316431@wipro.com>
2018-10-08 22:28:42 +0530

    login

---
+++ client/components/Login/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import services from '../../services'
+import './index.css'
+
+export default class PayOutPlan extends React.Component{
+  constructor(props){
+    super(props);
+    this.onLogin = this.onLogin.bind(this)
+  }
+onLogin(){
+  const username = document.getElementById("username").value
+  const pass = document.getElementById("pwd").value
+  var queryData = {
+    username:username,
+    password:pass
+  }
+  services.logincall(queryData,function (response) {
+    if(response.authenticated){
+      sessionStorage.setItem("username",username)
+      sessionStorage.setItem("token",response.token)
+      this.props.history.push("/accounts")
+    }else {
+      console.error("not authourised");
+    }
+  }.bind(this),function (err) {
+    throw(new Error(err.responseText))
+  })
+
+}
+    render(){
+        return(
+            <div className='llyods_login'>
+              <div className='logo_open_bank'>
+                OPEN BANKING
+              </div>
+              <div className='row' style={{paddingBottom:'20px'}}>
+                <div className='col-7'>
+                  <img style = {{height:'626px',width:'768px',paddingTop:'49px',paddingLeft:'38px'}} src = 'images/il-login-page.png' />
+                </div>
+                <div className='col-5' style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+                  <div className='Login_Background' style={{paddingTop:'49px',paddingLeft:'38px'}}>
+                    <div className='Sign_in_to_your_account'>
+                      Sign in to your account
+                    </div>
+                    <input type = 'text' className='field_outline' style={{marginTop:'34px'}}/>
+                    <input type = 'text' className='field_outline' style={{marginTop:'13.9px'}}/>
+                    <div style={{marginTop:'19.9px'}}>
+                      <div style={{float:'left',marginRight:'10px'}}>
+                        <input type='checkbox'/>
+                      </div>
+                      <div style={{}}>
+                        <div className=''>Keep me signed in</div>
+                      </div>
+                    </div>
+                    <div className='button_background' style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+                        SIGN IN
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        );
+    }
+}
--- client/index.js
+++ client/index.js
@@ -4,13 +4,13 @@ import {HashRouter, Route} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Balance from './components/Balances';
 
-
+import Login from './components/Login'
 
 render(
 <MuiThemeProvider>    
 <HashRouter>
     <div>
-        <Route exact path='/' component={Balance} />
+        <Route exact path='/' component={Login} />
  
 
     </div>
