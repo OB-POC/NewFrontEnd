75c8455c38cb8f9a7246012824bc4c1f98eb4fc5
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-10-09 21:37:52 +0530

    added auth

--- client/config.js
+++ client/config.js
@@ -2,12 +2,12 @@
 let NODE_ENV = NODE_ENV || ""
 
 module.exports = NODE_ENV == "PRO" ? {
-    loginUrl: 'http://localhost:8082',
+    loginUrl: 'http://localhost:8082/authService',
     accountUrl: 'http://localhost:8082/accountsService',
     payOutUrl: 'http://localhost:8082/payOutPlanService',
     offeringsUrl: 'http://localhost:8082/offeringService'
 } : {
-    loginUrl: 'http://localhost:3000',
+    loginUrl: 'http://localhost:3000/authService',
     accountUrl: 'http://localhost:3001/accountsService',
     payOutUrl: 'http://localhost:3002/payOutPlanService',
     offeringsUrl: 'http://localhost:3003/offeringService'
