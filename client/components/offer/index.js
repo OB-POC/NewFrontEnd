e9712dbcf860c0b8fedbbf592db18d436bd6104d
lbgopenbanking <prithvi.vuppalalpati@wipro.com>
2018-10-09 21:45:41 +0530

    offerings service done

--- client/components/offer/index.js
+++ client/components/offer/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Header from '../headernew';
 import Sidebar from '../sidebar';
+
+import services from '../../services'
 import './style.css';
 
 export default class Offerings extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            username:''
+            username:'',
+            cardData : []
         }
     }
+
+
+    componentWillMount(){
+        var context = this
+            var token = sessionStorage.getItem("token");
+          services.makePaymentCall(token,function (data) {
+            services.offeringCall(token,function (data1) {
+              context.setState({cardData : data1});
+              setTimeout(function(){
+                context.setState({timer: true,type:data.totalAvailableBalance});
+              }.bind(this),2000)
+            })
+          })
+        
+        }
 
     componentDidMount(){
         var username =
@@ -32,8 +50,8 @@ export default class Offerings extends React.Component{
                 </div>
                 <div id="bg" className="back_groud_panal">
                     <img src="images/img-imge-bg.png" className="bgimg"/>
-                    <h5 id="msg">Want to enjoy an annual savings of £4627.60? 
-                        Switch to LBG-Club Lloyds Saver Account at a new AER of 0.60%. </h5>
+                    <h5 id="msg">Want to enjoy an annual savings of <span>&#163;</span>{this.state.cardData.savingsOnBestMatch}? 
+                        Switch to LBG-Club Lloyds Saver Account at a new AER of {this.state.cardData.aer}%. </h5>
                     <img src="images/ic-card-copy.png" className="lbgcard"/>
                     <button className='switch-button' onClick={() => alert('Your transaction is being processed. You will receive confirmation message wihin 24 hrs.')}>
                         <span id="switch-msg">Switch Now</span>
