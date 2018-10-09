172cf662641f05112afe88f17b3a9d1e25bfb7e0
lbgopenbanking <prithvi.vuppalalpati@wipro.com>
2018-10-09 16:46:00 +0530

    tootip done

--- client/components/home/index.js
+++ client/components/home/index.js
@@ -37,7 +37,7 @@ export default class Rel extends React.Component{
   }
     render(){
         return(
-            <div  className='container-fluid' style={{paddingLeft:'0px',paddingRight:'0px'}}>
+            <div className='container-fluid' style={{paddingLeft:'0px',paddingRight:'0px'}}>
               <Header username = {this.state.accSumary.username}/>
               <div style = {{display:"flex"}}>
                 <Sidebar/>
@@ -47,7 +47,7 @@ export default class Rel extends React.Component{
                 <Balance creditData = {this.state.creditData} debitData = {this.state.debitData}/>
                 </div>
                 <div className='col-3'>
-                <img style = {{height:'670px',width:'260px',paddingTop:'16%',marginTop:'18%'}} src='images/image-ad@3x.png'/>
+                <img style = {{height:'700px',width:'260px',paddingTop:'16%',marginTop:'18%'}} src='images/image-ad@3x.png'/>
                 </div>
                 </div>
               </div>
