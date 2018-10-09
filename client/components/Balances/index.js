2e27eca83fb68ffd4e52bcc22e3c873a876affea
lbgopenbanking <prithvi.vuppalalpati@wipro.com>
2018-10-09 15:47:39 +0530

    servies bug fixed

--- client/components/Balances/index.js
+++ client/components/Balances/index.js
@@ -5,13 +5,27 @@ import './style.css';
 
 
 export default class Balance extends React.Component{
+
+    componentWillMount(){
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip({
+            title: function() {
+                var element = $( this );
+                if ( element.is( ".outer-layer" ) ) {
+                    return ("<img style='height:110px,width:175px,margin-left:22px' src='images/card_img1.jpg'/></div>div class='col-4' style='padding-top:30px,padding-left:55px'><div style='display:flex,flex-direction:row,margin-right:22px'><div style='display:flex,flexDirection:column'><div class='bank'>Banks</div><div class='JP_Morgan_Chase'>JP Morgan Chase</div><div class='bank' style='margin-top:15px'>Credit limit</div><div class='JP_Morgan_Chase'>£ 3000</div><div class='bank' style='margin-top:15px'>Available credit</div><div class='JP_Morgan_Chase'>£ 1200</div></div></div> </div><div class='col-4' style='padding-top:30px,padding-left:22px,padding-right:22px'><div style='display:flex,flex-direction:row'><div style='display:flex,flex-direction:column'><div class='bank'>Minimum due balance</div><div class='JP_Morgan_Chase'>£ 30</div><div class='bank' style='margin-top:15px'>Due date</div><div class='JP_Morgan_Chase' style='color:#ff5d64'>Thursday, Oct 11, 2018</div></div></div></div></div></div>"
+                      );
+                }           
+            }, html: true ,placement: "top"});
+        });
+     }
+
     render(){
         return(
             <div>
                 <div style={{display:'flex',marginLeft:'65px'}}>
                 <Paper className='paper' zDepth={2} style = {{padding:'15px'}}>
                    <div className='credit-accounts'>Credit accounts</div>
-                   <div className='outer-layer' style={{display:'flex'}}>
+                   <div className='outer-layer' style={{display:'flex'}} data-toggle="tooltip" data-placement="top" >
                    <div className='img-credit'><img src='../../../../images/card_img1.jpg' /></div>
                    <div className='detail-credit'>
                         <p className='credit'>Halifax<br/>
