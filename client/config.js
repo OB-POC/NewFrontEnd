e1788db8bcbe9eb86786b37366552019cb3ef5d3
nitinmaram <nitinmaram@gmail.com>
2018-10-09 20:42:56 +0530

    resolved services

--- client/config.js
+++ client/config.js
@@ -3,12 +3,12 @@ let NODE_ENV = NODE_ENV || ""
 
 module.exports = NODE_ENV == "PRO" ? {
     loginUrl: 'http://localhost:8082/authService',
-    accountUrl: 'http://localhost:8082/accountsService',
+    accountUrl: 'http://localhost:8082',
     payOutUrl: 'http://localhost:8082/payOutPlanService',
     offeringsUrl: 'http://localhost:8082/offeringService'
 } : {
     loginUrl: 'http://localhost:3000/authService',
-    accountUrl: 'http://localhost:3001/accountsService',
+    accountUrl: 'http://localhost:3001',
     payOutUrl: 'http://localhost:3002/payOutPlanService',
     offeringsUrl: 'http://localhost:3003/offeringService'
 };
