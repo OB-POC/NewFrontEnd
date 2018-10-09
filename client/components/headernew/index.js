a0ec79e5a3df3e30d2425696f01ee407d01abb9c
vinitkumargoel <vinit928@gmail.com>
2018-10-09 15:15:14 +0530

    bug fixed

--- client/components/headernew/index.js
+++ client/components/headernew/index.js
@@ -13,20 +13,21 @@ export default class Header extends React.Component{
         return(
             <div>
               <div className = 'Rectangle-2' style = {{display:'flex',justifyContent:'space-between'}}>
-              <div style = {{padding:"20px"}}>
-              <span className = 'OPEN-BANK'>OPEN BANKING</span>
-              </div>
-              <div style= {{display:"flex",paddingRight:'50px'}}>
-              <div style= {{padding:"15px",borderRight:'solid 0.5px #979797'}}>
-              <img className = "notification-1"src = 'images/not.png' />
-              </div>
-              <div style= {{padding:"15px"}}>
-              <img className = "img_user" src = 'images/img-u.png' />
-              </div>
-              <div style= {{padding:"17px",paddingLeft:'0',display:'flex'}}>
-              <span className = 'Alice-Salas' style = {{paddingRight:'18px'}}>{this.props.username}</span>
-              <i class="fas fa-caret-down"></i>
-              </div>
+                <div style = {{padding:"20px"}}>
+                  <span className = 'OPEN-BANK'>OPEN BANKING</span>
+                </div>
+                <div style= {{display:"flex",paddingRight:'50px'}}>
+                  <div style= {{padding:"15px",borderRight:'solid 0.5px #979797'}}>
+                    <img className = "notification-1"src = 'images/not.png' />
+                  </div>
+                  <div style= {{padding:"15px"}}>
+                    <img className = "img_user" src = 'images/img-u.png' />
+                  </div>
+                  <div style= {{padding:"17px",paddingLeft:'0',display:'flex'}}>
+                    <span className = 'Alice-Salas' style = {{paddingRight:'18px'}}>{this.props.username}</span>
+                    <i class="fas fa-caret-down"></i>
+                  </div>
+                </div>
               </div>
             </div>
         );
