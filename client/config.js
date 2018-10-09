fec779e835f79291c18474348a568ae8016602d9
vinitkumargoel <vinit928@gmail.com>
2018-10-09 16:33:38 +0530

    dynamic ENV

--- client/config.js
+++ client/config.js
@@ -1,6 +1,13 @@
-module.exports = {
-loginUrl: 'http://localhost:3000',
-accountUrl: 'http://localhost:3001',
-payOutUrl: 'http://localhost:3002',
-offeringsUrl: 'http://localhost:3003'
-}
+console.log(NODE_ENV);
+
+module.exports = NODE_ENV == "PRO" ? {
+    loginUrl: 'http://localhost:8082/authService',
+    accountUrl: 'http://localhost:8082/accountsService',
+    payOutUrl: 'http://localhost:8082/payOutPlanService',
+    offeringsUrl: 'http://localhost:8082/offeringService'
+} : {
+    loginUrl: 'http://localhost:3000',
+    accountUrl: 'http://localhost:3001',
+    payOutUrl: 'http://localhost:3002',
+    offeringsUrl: 'http://localhost:3003'
+};
