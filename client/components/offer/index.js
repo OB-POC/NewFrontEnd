a498fad69eb846ba8bc0a448f7b64bcad319cd66
nitinmaram <nitinmaram@gmail.com>
2018-10-10 17:03:38 +0530

    offering services toaster

--- client/components/offer/index.js
+++ client/components/offer/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Header from '../headernew';
 import Sidebar from '../sidebar';
 import ReactLoading from 'react-loading'
-
+import { ToastContainer, toast, Slide, Zoom, Flip, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import services from '../../services'
 import './style.css';
+import { css } from 'glamor';
 
 export default class Offerings extends React.Component{
     constructor(props){
@@ -12,9 +14,13 @@ export default class Offerings extends React.Component{
         this.state={
             username:'',
             cardData : [],
-            load: false
+            load: false,
+            load1: false,
+            toastId: null
         }
     }
+
+
 
 
     componentWillMount(){
@@ -43,7 +49,28 @@ export default class Offerings extends React.Component{
           load: true
         })
       }
+
+      notify() {
+        this.setState({
+          load1: true
+        })
+        setTimeout(this.fun.bind(this), 2000)
+      }
+      fun(){
+        this.setState({
+          load1: false
+        })
+        toast.success(`Your transaction is being processed. You will receive confirmation
+          message wihin 24 hrs. !`, {
+            className: css({
+          background: '#006a4d'
+        }),
+        position: toast.POSITION.BOTTOM_CENTER
+      });
+      }
+
     render(){
+
       return(
           this.state.load ?
             <div>
@@ -56,19 +83,24 @@ export default class Offerings extends React.Component{
                     <img src="images/img-banner.png" id="banner-img"/>
                     <h4 id="lable">Your payment is complete!</h4>
                 </div>
+                {this.state.load1 ? <ReactLoading type='bubbles' color='black' height={'20%'} width={'20%'} />
+                :null
+              }
                 <div id="bg" className="back_groud_panal">
                     <img src="images/img-imge-bg.png" className="bgimg"/>
                     <h5 id="msg">Want to enjoy an annual savings of <span>&#163;</span>{this.state.cardData.savingsOnBestMatch}?
                         Switch to LBG-Club Lloyds Saver Account at a new AER of {this.state.cardData.aer}%. </h5>
                     <img src="images/ic-card-copy.png" className="lbgcard"/>
-                    <button className='switch-button' onClick={() => alert('Your transaction is being processed. You will receive confirmation message wihin 24 hrs.')}>
+                    <button className='switch-button' style = {{cursor: 'pointer'}} onClick={this.notify.bind(this)}>
                         <span id="switch-msg">Switch Now</span>
                         <i id="right-arrow" className='fas fa-arrow-right fa-lg'></i>
                     </button>
                 </div>
+
             </div>
             </div>
             </div>
+            <ToastContainer autoClose={4000} closeButton={false} hideProgressBar={true} transition={Zoom}/>
             </div>:
             <div>
             <center><ReactLoading type='bubbles' color='black' height={'20%'} width={'20%'} /></center>
