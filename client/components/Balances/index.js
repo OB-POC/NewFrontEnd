991aaac5285cff6311d0a0333a1b96e595514803
nitinmaram <nitinmaram@gmail.com>
2018-11-20 16:16:31 +0530

    removed conflict

--- client/components/Balances/index.js
+++ client/components/Balances/index.js
@@ -230,7 +230,7 @@ export default class Balance extends React.Component{
                     </div>
                 </div>
                     {debitData}
-<<<<<<< HEAD
+
                     <center id = 'outer-layer'>
                     <button className='btn payout-button optimize-btt optimizeBtn' onClick = {this.handleClick.bind(this)}>
                       <div className="btn-text-wrapper">
@@ -241,8 +241,7 @@ export default class Balance extends React.Component{
                       </div>
                     </button>
                   </center>
-=======
->>>>>>> 2421a87659b0d96434e0e7644e47342fc27b7ed7
+
                 </Paper>
                 <Paper className='paper' zDepth={2} style = {{padding:'20px',width: '50%'}}>
                    <div className='row credit-accounts' style = {{paddingBotttom:'23px'}}>
