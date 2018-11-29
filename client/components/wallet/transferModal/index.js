633d83f7c6f61ba5c7cb1fa379de1d886c3cb2bb
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-11-29 19:06:15 +0530

    success modal

--- client/components/wallet/transferModal/index.js
+++ client/components/wallet/transferModal/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 import { Button, Header, Icon, Modal, Table, ListHeader } from 'semantic-ui-react'
-
+import SuccessModal from './success'
 export default class CustomModal extends React.Component{
     constructor(props){
       super(props);
@@ -9,32 +9,26 @@ export default class CustomModal extends React.Component{
         modalOpen: false,
         linesArr : []
         }
-        let ref = React.createRef()
     }
-    componentDidMount(){
-        console.log(this.ref)
-        console.log(this.refs)
-        this.setState((prevState,props)=>{
 
-            let linesArr = prevState.linesVar
-            let lineVar = new LeaderLine(this.refs.fromRef.id,this.refs.toRef.id)
-        })
-    }
     handleOpen = () => this.setState({ modalOpen: true })
     handleClose = () => this.setState({ modalOpen: false })
       render(){
-        
+        console.log(this.props.transferFundsButton,"transferFundsButton...",this.props.children);
           return(
          <Modal
-            trigger={<span onClick={this.handleOpen} className = "View-details" style = {{cursor:'pointer'}}>View details  <i class="fas fa-arrow-right"></i></span>}
+            trigger={<div className = 'Rectangle-5'>
+            TRANSFER FUNDS &nbsp;&nbsp;<i className="fas fa-arrow-right"></i>
+            </div>}
+            onOpen = {this.handleOpen}
             size='small'
             style = {{margin:'auto',height:'70.5vh',marginTop: 'auto',backgroundColor: '#f5f6fa',padding:'8.7vh 6.7vh',borderRadius:'14px'}}
-              open={this.state.modalOpen}>
-              <div><Icon name='close' onClick = {this.handleClose}/></div>
+              open={this.state.modalOpen} >
+              <div><Icon name='close' onClick = {this.props.handleClose}/></div>
             <p className = 'Payment-instructions'>Transfer Funds</p>
-            
+
             <p className="transfer-funds-modal-message">Allow Optima to transfer £ 100 from Munzo account to RBS?</p>
-            <div ref={(node)=>{this.fromRef=node}} style={{display:'flex',justifyContent:'flex-start',marginTop:'20px',marginBottom:'30px'}}>
+            <div  style={{display:'flex',justifyContent:'flex-start',marginTop:'20px',marginBottom:'30px'}}>
                 <div className='from-card' style={{display:'flex',justifyContent:'center',alignItems:'center',marginRight:'151px',padding:'10px'}}>
                     <img src = {"./images/img-mozo.png"||"./images/cards/debit/"+{}+"@3x.png"} style={{width:'81px',height:'51px',marginRight:'14px'}}/>
                     <p className='bank-name' style={{marginRight:'40px',marginBottom:'0px'}}>Monzo</p>
@@ -45,10 +39,8 @@ export default class CustomModal extends React.Component{
                 <p className='bank-name'>Monzo</p>
                 </div>
             </div>
-            <div ref={this.ref} style={{display:'flex',justifyContent:'flex-start'}}>
-                <div className='yes-button' onClick={this.handleClose} style={{display:'flex',justifyContent:'center',alignItems:'center',marginRight:'20px',cursor:'pointer'}}>
-                <p className='yes-text-style' style={{color:'#FFFFFF'}}>YES</p>
-                </div>
+            <div style={{display:'flex',justifyContent:'flex-start'}}>
+                <SuccessModal />
                 <div  onClick={this.handleClose} className='cancel-button' style={{display:'flex',justifyContent:'center',alignItems:'center',cursor:'pointer',border:'1px solid #4a4a4a'}}>
                 <p className='yes-text-style' style={{color:'#4a4a4a',}}>CANCEL</p>
                 </div>
