9f5ddd8438a6565e35df6ca3bcc1ff8b561bea6c
nitinmaram <nitinmaram@gmail.com>
2018-11-29 22:11:05 +0530

    pooling funds

--- client/components/PoolFunds/FromCards/index.js
+++ client/components/PoolFunds/FromCards/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import {Link} from 'react-router-dom';
 import Header from '../../headernew'
 import Sidebar from '../../sidebar'
+import Services from '../../../services'
 
 export default class PoolingFromCard extends React.Component{
   constructor(props){
@@ -13,6 +14,15 @@ export default class PoolingFromCard extends React.Component{
       creditData : [],
       accSumary: {}
     }
+  }
+  componentDidMount(){
+    var token = sessionStorage.getItem("token");
+    Services.debitCall(token,function(data){
+         this.setState({debitData : data.banks});
+         console.log(data.banks);
+     }.bind(this),function(err){
+         console.log(err);
+     })
   }
 
   handleChange = (e, { value }) => this.setState({ value })
@@ -28,14 +38,19 @@ export default class PoolingFromCard extends React.Component{
     }
     console.log('Next');
   }
-
+cardClick = (i) => {
+  console.log(i);
+  this.setState({
+    ['card'+i] : !this.state['card'+i]
+  })
+}
     render(){
         return(
           <div className='container-fluid' style={{paddingLeft:'0px',paddingRight:'0px'}}>
               <Header username = {this.state.accSumary.username} history = {this.props.history}/>
             <div style = {{display:"flex"}}>
                 <Sidebar activeComponent = "wallet"/>
-              <div className='main-content' style = {{backgroundColor:"#f5f6fa",width:"94.5%",paddingBottom:'20px'}}>
+              <div className='main-content' style = {{backgroundColor:"#f5f8fa",width:"94.5%",paddingBottom:'20px'}}>
                 <div>
                 <h1 style = {{fontWeight: '300',marginTop:'20PX'}}>My Accounts</h1>
                 </div>
@@ -51,64 +66,72 @@ export default class PoolingFromCard extends React.Component{
                     </div>
                     <div className='pool-subheader'>Select accounts to which you want to pool your funds</div>
                   <div className='row'>
-                    <div className='pool-card' style={{marginRight:'30px'}}>
-                        <div>
-                            <img src = {"./images/img-card.png"} style = {{ width: '151px',height: '93px',margin:'23px'}}/>
-                          </div>
-                         <div>
-                             <div className='row' style={{marginTop:'-20px'}}>
-                                 <div className='col-6'>
-                                 <div className='pool-bankName'>
-                                   bankName
-                                 </div>
-                                 <div className='pool-aer_subscript'>
-                                    0.3% AER
-                                 </div>
-                                 </div>
-                                 <div className='col-6'/>
-                             </div>
-                             <div className='row' style={{marginTop:'28px'}}>
-                                 <div className='col-6' >
-                                     <div className='pool-balance'>
-                                         Available Balance
-                                     </div>
-                                     <div className='pool-amount' style={{marginTop:'5px'}}>
-                                     balance
-                                     </div>
-                                 </div>
-                              </div>
-                        </div>
-                     </div>
-                     <div className='pool-card' style={{ boxShadow: '0 14px 37px 0 rgba(0, 106, 77, 0.16)',backgroundColor:'rgba(0, 106, 77, 0.14)'}}>
-                    <i className="fas fa-check-circle fa-lg float-right" style={{marginTop:'-5px',marginRight:'10px',color:'#2d8259'}}></i>
-                          <div>
-                            <img src = {"./images/img-card.png"} style = {{ width: '151px',height: '93px',margin:'23px'}}/>
-                          </div>
-                         <div>
-                             <div className='row' style={{marginTop:'-20px'}}>
-                                 <div className='col-6'>
-                                 <div className='pool-bankName'>
-                                   bankName
-                                 </div>
-                                 <div className='pool-aer_subscript'>
-                                    0.3% AER
-                                 </div>
-                                 </div>
-                                 <div className='col-6'/>
-                             </div>
-                             <div className='row' style={{marginTop:'28px'}}>
-                                 <div className='col-6' >
-                                     <div className='pool-balance'>
-                                         Available Balance
-                                     </div>
-                                     <div className='pool-amount' style={{marginTop:'5px'}}>
-                                     balance
-                                     </div>
-                                 </div>
-                              </div>
-                        </div>
+                  {this.state.debitData.map((val,i) =>{
 
-                     </div></div>
+                    return(
+                      !this.state['card'+i] ?
+
+                      <div className='pool-card' style={{marginRight:'30px'}} tabIndex = "1" onClick = {this.cardClick.bind(this,i)}>
+                          <div>
+                              <img src = {"./images/img-card.png"} style = {{ width: '151px',height: '93px',margin:'23px'}}/>
+                            </div>
+                           <div>
+                               <div className='row' style={{marginTop:'-20px'}}>
+                                   <div className='col-8'>
+                                   <div className='pool-bankName'>
+                                     {val.bankName}
+                                   </div>
+                                   <div className='pool-aer_subscript'>
+                                      {val.accounts[0].interestRate} % AER
+                                   </div>
+                                   </div>
+                                   <div className='col-8'/>
+                               </div>
+                               <div className='row' style={{marginTop:'28px'}}>
+                                   <div className='col-8' >
+                                       <div className='pool-balance'>
+                                           Available Balance
+                                       </div>
+                                       <div className='pool-amount' style={{marginTop:'5px'}}>
+                                       £ {val.accounts[0].availableBalance}
+                                       </div>
+                                   </div>
+                                </div>
+                          </div>
+                       </div>:
+                       <div className='pool-card' tabIndex = "1" onClick = {this.cardClick.bind(this,i)} style={{ boxShadow: '0 14px 37px 0 rgba(0, 108, 77, 0.18)',backgroundColor:'rgba(0, 108, 77, 0.14)'}}>
+                      <i className="fas fa-check-circle fa-lg float-right" style={{marginTop:'-5px',marginRight:'10px',color:'#2d8259'}}></i>
+                            <div>
+                              <img src = {"./images/img-card.png"} style = {{ width: '151px',height: '93px',margin:'23px'}}/>
+                            </div>
+                           <div>
+                               <div className='row' style={{marginTop:'-20px'}}>
+                                   <div className='col-8'>
+                                   <div className='pool-bankName'>
+                                     {val.bankName}
+                                   </div>
+                                   <div className='pool-aer_subscript'>
+                                      {val.accounts[0].interestRate} % AER
+                                   </div>
+                                   </div>
+                                   <div className='col-8'/>
+                               </div>
+                               <div className='row' style={{marginTop:'28px'}}>
+                                   <div className='col-8' >
+                                       <div className='pool-balance'>
+                                           Available Balance
+                                       </div>
+                                       <div className='pool-amount' style={{marginTop:'5px'}}>
+                                       £ {val.accounts[0].availableBalance}
+                                       </div>
+                                   </div>
+                                </div>
+                          </div>
+                       </div>
+
+                    )
+                  })}
+                     </div>
                      <div className='pool-line'></div>
                      <div className = "flex-container">
                       <Link to='/wallet'><div className="flex-item" onClick = {this.onCancelClick}>CANCEL</div></Link>
